154ecd0a39524feb2a5440e30bd93911f2d65888
everduvan <everduvanhdez@gmail.com>
2022-02-11 09:14:37 -0500

    second

--- pythonProject1/impares.py
+++ pythonProject1/impares.py
@@ -1,3 +1,33 @@
 n = 10
 impares = [n for n in range (n+1) if n%2 !=0]
 print(*impares, sep=", ")
+
+
+print ('generadores')
+
+
+def pares (maximo):
+    num=0
+    lista_pares=[]
+    while num<maximo:
+        lista_pares.append(num*2)
+        num+=1
+    return lista_pares
+
+num_pares=pares(10)
+print (num_pares)
+
+print ('*************************************')
+
+def paresgen(max):
+    num1=0
+    while num1<max:
+        yield num1*2
+        num1+=1
+
+
+num1pares=paresgen(10)
+print(next(num1pares))
+print(next(num1pares))
+print(next(num1pares))
+print(next(num1pares))
--- webscraping/main.py
+++ webscraping/main.py
@@ -1,2 +1,3 @@
-from webscraping import *
-
+text = 'https://www.stratanetworks.com/galaxy-s10-plus'
+text_hash = hash(text)
+print (text_hash)
