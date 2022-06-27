80822395205fbb929586b9830bde3f8d70f7a2ef
EverDuvan <everduvanhdez@gmail.com>
2022-06-27 14:19:59 -0500

    update

--- catalogo_peliculas/dominio/Pelicula.py
+++ catalogo_peliculas/dominio/Pelicula.py
@@ -1,3 +1,31 @@
+from mailbox import NoSuchMailboxError
+
+
 class Pelicula:
-    def __init__(self) -> None:
-        pass
+    contador_peliculas = 0
+
+    def __init__(self, nombre):
+        Pelicula.contador_peliculas += 1
+        self._id_pelicula = Pelicula.contador_peliculas
+        self._nombre = nombre
+
+    @property
+    def id_pelicula(self):
+        return self._id_pelicula
+
+    @id_pelicula.setter
+    def id_pelicula(self, id_pelicula):
+        self._id_pelicula = id_pelicula 
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre 
+
+    def __str__(self):
+        return f'Pelicula id: {self._id_pelicula}, Nombre: {self._nombre}'
+
+
