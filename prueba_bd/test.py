b93bd4fa7e34b0d9dbe7bb4b276b00b4bdc09776
EverDuvan <everduvanhdez@gmail.com>
2022-07-12 20:47:11 -0500

    base de datos

---
+++ prueba_bd/test.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+conexion = psycopg2.connect(user = 'everduvan', password = '030481', host = 'localhost', port = '5432', database = 'data_procesing2')
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            # SELECT * FROM _ en la sentencia se escribe la tabla que se trae homologados en este caso
+            # WHERE = URL %s _ para escribir la fila especifica a traer, URL en este caso
+            sentencia = 'SELECT * FROM homologados WHERE category_id = %s'
+            category_id = input ('escriba el numero de category_id: ')
+            cursor.execute(sentencia, (category_id,))
+            # fetchall = regresa todos los registros 
+            # fetchone = regresa un solo registro 
+            registros = cursor.fetchall() 
+            for registro in registros:
+                print(registros)
+except Exception as e:
+    print(f'Ocurrió el siguiente error: {e}')
+finally:
+    conexion.close()
+
+
