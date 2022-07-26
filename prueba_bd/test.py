06d9a38b3d33826dac14c01153c1ed42eaad049c
EverDuvan <everduvanhdez@gmail.com>
2022-07-26 09:31:03 -0500

    bd

--- prueba_bd/test.py
+++ prueba_bd/test.py
@@ -1,14 +1,14 @@
 import psycopg2
 
-conexion = psycopg2.connect(user = 'xxxx', password = 'xxxx', host = 'localhost', port = '5432', database = 'data_procesing2')
+conexion = psycopg2.connect(user = 'postgres', password = '030481', host = 'localhost', port = '5432', database = 'db_prueba')
 
 try:
     with conexion:
         with conexion.cursor() as cursor:
             # SELECT * FROM _ en la sentencia se escribe la tabla que se trae homologados en este caso
             # WHERE = URL %s _ para escribir la fila especifica a traer, URL en este caso
-            sentencia = 'SELECT * FROM homologados WHERE category_id = %s'
-            category_id = input ('escriba el numero de category_id: ')
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
+            category_id = input ('escriba el numero de id : ')
             cursor.execute(sentencia, (category_id,))
             # fetchall = regresa todos los registros 
             # fetchone = regresa un solo registro 
