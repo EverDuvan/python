76770368e292199bc526c2ceb548d436af6aabd6
EverDuvan <everduvanhdez@gmail.com>
2022-08-04 16:21:23 -0500

    update

--- capa_datos_persona/conexion.py
+++ capa_datos_persona/conexion.py
@@ -32,40 +32,25 @@ class Conexion:
     
     @classmethod
     def obtenerConexion(cls):
-        pass
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexion obtenida del pool: {conexion}')
+        return conexion
 
-        '''         
-        if cls._conexion is None:
-            try:
-                cls._conexion  = bd.connect(host=cls._HOST, 
-                                            user=cls._USERNAME, 
-                                            password=cls._PASSWORD, 
-                                            port=cls._DB_PORT, 
-                                            database=cls._DATABASE)
-                log.debug(f'Conexion exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.ERROR(f'Ocurrió un error al obtener la conexion {e}')
-                sys.exit()
-        else:
-            return cls._conexion
-        '''
-    '''   
     @classmethod
-    def obtenerCursor(cls):
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.ERROR(f'Ociurrió una excepcion al obtener un cursor {e}')
-                sys.exit()
-        else:
-            return cls._cursor
-    '''
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexion al pool: {conexion}')
+
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
 
 if __name__ == '__main__':
-    pass
-
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+    conexion2 = Conexion.obtenerConexion()
+'''     conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+ '''
 
