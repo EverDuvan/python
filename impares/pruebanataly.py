50f7e8a6890e140ffa1c36b71512f27186cd1b7e
everduvan <everduvanhdez@gmail.com>
2022-12-14 10:27:30 -0500

    update

---
+++ impares/pruebanataly.py
@@ -0,0 +1,8 @@
+print ('digite su primer numero')
+numero1 = int(input())
+print ('digite su segundo numero')
+numero2 = int(input())
+numero3 = 2
+resultado = numero1 * numero2 / numero3
+
+print (f'el reusltado de num1 * num2 y dividido entre 2 es :{resultado}')
