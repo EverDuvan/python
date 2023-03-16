d9d2914526c2d97a2950c13cb300e2e738c06e52
everduvan <everduvanhdez@gmail.com>
2023-03-16 09:40:10 -0500

    update

--- nunpy_pandas/numpy01.py
+++ nunpy_pandas/numpy01.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import datetime
 
 #lista_de_listas=[[1,-4],[12,3],[7.2,5]]
 
@@ -7,3 +9,13 @@ lista_de_listas=[[1  ,-4],[12 , 3],[7.2, 5]]
 
 matriz = np.array(lista_de_listas)
 print(matriz)
+
+## df con la fecha y hora actual
+
+now = datetime.datetime.now()
+data = {'fecha': [now.strftime('%Y-%m-%d')],
+        'hora': [now.strftime('%H:%M:%S')]}
+df = pd.DataFrame(data)
+print(df)
+df = df.reindex(columns=['hora', 'fecha'])
+print (df)
